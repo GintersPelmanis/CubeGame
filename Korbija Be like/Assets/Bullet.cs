39258590708041653845245ba043965aa185234a
tortilja20 <67796234+tortilja20@users.noreply.github.com>
2021-02-10 17:12:42 +0200

    Pirmais
    sd

---
+++ Korbija Be like/Assets/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    public float damage = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<Health>().TakeDamage(damage);
+        Destroy(gameObject);
+        
+    }
+}
---
+++ Korbija Be like/Assets/Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+
+    public float max_health = 100f;
+    public float curent_health = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curent_health = max_health;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TakeDamage(float amount)
+    {
+        curent_health -= amount;
+    
+        if (curent_health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
